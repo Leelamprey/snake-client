6ac00d3619f9768e55d603e17496f1dc3831c53c
Lee Benzacar <LMBenzacar@gmail.com>
2022-01-19 21:59:33 +0000

    added setupInput

--- play.js
+++ play.js
@@ -1,7 +1,28 @@
 
+const { stdin } = require('process');
 const {connect} = require('./client.js');
 // establishes a connection with the game server
 
 
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      console.log("Closing game!");
+      process.exit();
+    }
+  };
+  
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
 console.log("Connecting ...");
 connect();
+setupInput();
