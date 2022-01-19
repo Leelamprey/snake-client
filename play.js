f0f3f8902b0d6bf9f799f28638a9e956e754f4a1
Lee Benzacar <LMBenzacar@gmail.com>
2022-01-19 22:16:37 +0000

    Movement

--- play.js
+++ play.js
@@ -1,28 +1,9 @@
 
 const { stdin } = require('process');
 const {connect} = require('./client.js');
+const {setupInput} = require('./input.js');
 // establishes a connection with the game server
 
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      console.log("Closing game!");
-      process.exit();
-    }
-  };
-  
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-
-
 console.log("Connecting ...");
-connect();
-setupInput();
+let connection = connect();
+setupInput(connection);
