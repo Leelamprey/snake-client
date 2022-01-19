3bd9dc1db9133e07f4f7b619215e235c8a57db2c
Lee Benzacar <LMBenzacar@gmail.com>
2022-01-19 22:30:08 +0000

    constants

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 const connect = function (data) {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541'
+    host: IP,
+    port: PORT
   });
   conn.on('connect', (data) => {
     console.log('Successfuly connected to game server!');
